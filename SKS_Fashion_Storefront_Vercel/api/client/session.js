f532accb660fe861dd3a02f60f17c4b8496b2c54
praveen dinusara <praveendinusara@gmail.com>
2026-08-01 18:35:00 +0530

    Delete SKS_Fashion_Storefront_Vercel/api/client/session.js

--- SKS_Fashion_Storefront_Vercel/api/client/session.js
+++
@@ -1,2 +0,0 @@
-import { requireSalesClient } from "../../lib/auth.js";
-export default function handler(request, response) { if (request.method !== "GET") return response.status(405).json({ error: "Method not allowed" }); const session = requireSalesClient(request, response); if (session) response.status(200).json({ username: session.username, csrfToken: session.csrfToken }); }
